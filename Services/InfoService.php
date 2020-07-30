6a6377b5dd30a0ca5894b69b0940286ddedd1537
Your Name <you@example.com>
2020-07-30 16:07:44 -0300

    .

---
+++ Services/InfoService.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\ValidationShippingCompany\Services; 
+
+
+use Modules\Portal\Rules\NotInCustomRule;
+use Modules\Portal\Services\Validation\Data\InfoValidationService;
+
+
+class InfoService extends InfoValidationService
+{
+
+	public function rule($data, $index, $columns){
+		return  [
+			'id' => ['integer', 'min:1', new NotInCustomRule(InfoValidationService::chunkColumn($columns, 'id', 0, $index-2), 'Duplicado')],
+			'name' => ['filled', 'string', 'max:191', new NotInCustomRule(InfoValidationService::chunkColumn($columns, 'name', 0, $index-2), 'Duplicado')]
+		];
+	}
+
+	public function modifiers()
+	{
+		return [];
+	}
+
+	public function columnsFormat()
+	{
+		return [];
+	}
+
+}
